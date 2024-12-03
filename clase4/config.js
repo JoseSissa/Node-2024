de671f1b5ac8631331be68ae86a07ab81f8b7ffb
Jose Sissa <sissamendoza@outlook.com>
2024-12-03 18:06:15 -0500

    JWT and conditional rendering

--- clase4/config.js
+++ clase4/config.js
@@ -1,4 +1,7 @@
+// SECRET_JWT_KEY
+// Procurar en el archivo .env y mucho más robusto que 'secret'
 export const {
     PORT = 3000, 
-    SALT_ROUNDS = 4 
+    SALT_ROUNDS = 4 ,
+    SECRET_JWT_KEY = 'secret'
 } = process.env
--- clase4/index.js
+++ clase4/index.js
@@ -1,14 +1,25 @@
 import express from 'express'
-import { PORT } from './config.js'
+import { PORT, SECRET_JWT_KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const token = req.cookies.access_token
+    if (!token) return res.render('index')
+
+    try {
+        const data = jwt.verify(token, SECRET_JWT_KEY)
+        res.render('index', data)
+    } catch (error) {
+        res.render('index')
+    }
 })
 
 // Register
@@ -26,13 +37,24 @@ app.post('/register', async (req, res) => {
 
 // Login
 app.post('/login', async (req, res) => {
-    const { username, password } = req.body
-    console.log({ username, password })
-    
+    const { username, password } = req.body    
 
     try {
         const user = await UserRepository.login({ username, password })
-        res.send({ user })
+        const token = jwt.sign(
+            { id: user.id, username: user.username },
+            SECRET_JWT_KEY,
+            { expiresIn: '1h' }
+        )
+        res
+            .cookie('access_token', token, 
+                { 
+                    httpOnly: true, // La cookie solo se puede acceder en el servidor
+                    secure: process.env.NODE_ENV === 'production', // Solo se puede acceder en producción con HTTPS
+                    sameSite: 'strict', // La cookie solo se puede desde el mismo dominio
+                    maxAge: 1000 * 60 * 60 // La cookie expira en 1 hora
+                })
+            .send({ user, token })
     } catch (error) {
         // Normalmente no se debería enviar el error al cliente
         res.status(401).send(error.message)
@@ -42,7 +64,17 @@ app.post('/login', async (req, res) => {
 
 app.post('/logout', (req, res) => { })
 
-app.post('/protected', (req, res) => { })
+app.get('/protected', (req, res) => {
+    const token = req.cookies.access_token
+    if (!token) return res.status(403).send('Access denied')
+
+    try {
+        const data = jwt.verify(token, SECRET_JWT_KEY)
+        res.render('protected', data)
+    } catch (error) {
+        res.status(401).send('Access not allowed')
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
