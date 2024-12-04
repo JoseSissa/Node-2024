a4ccc8c83cb3ecf5e47d22793589b2c392a6fec1
Jose Sissa <sissamendoza@outlook.com>
2024-12-04 16:48:50 -0500

    Logout completed and redirect

--- clase4/index.js
+++ clase4/index.js
@@ -10,16 +10,22 @@ app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
+app.use((req, res, next) => {
     const token = req.cookies.access_token
-    if (!token) return res.render('index')
-
+    req.session = { user: null }
     try {
-        const data = jwt.verify(token, SECRET_JWT_KEY)
-        res.render('index', data)
+        const data = jwt.verify(token, SECRET_JWT_KEY)  
+        req.session.user = data
     } catch (error) {
-        res.render('index')
+        req.session.user = null
     }
+
+    next() // Seguir a la sgte ruta o middleware
+})
+
+app.get('/', (req, res) => {
+    const { user } = req.session
+    res.render('index', { user })
 })
 
 // Register
@@ -62,18 +68,19 @@ app.post('/login', async (req, res) => {
 })
 
 
-app.post('/logout', (req, res) => { })
+app.post('/logout', (req, res) => {
+    console.log('hols logout');
+    
+    res
+        .clearCookie('access_token')
+        .json({ message: 'Logged out' })
+        .redirect('/')
+});
 
 app.get('/protected', (req, res) => {
-    const token = req.cookies.access_token
-    if (!token) return res.status(403).send('Access denied')
-
-    try {
-        const data = jwt.verify(token, SECRET_JWT_KEY)
-        res.render('protected', data)
-    } catch (error) {
-        res.status(401).send('Access not allowed')
-    }
+    const { user } = req.session
+    if (!user) return res.status(403).send('Access denied')        
+    res.render('protected', { user })
 })
 
 app.listen(PORT, () => {
